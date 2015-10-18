4751083f5a3d7684924126a15192058486fe04d2
hupeng <hupeng1982@126.com>
2015-10-18 13:33:10 +0800

    finished Invigilator class.

--- api/get_command.php
+++ api/get_command.php
@@ -1,74 +1,24 @@
 <?php
 require '../config/path.php';
-require_once PATH_CONFIG . 'value.php';
-require_once PATH_LIB . 'Logger.php';
-require_once PATH_LIB . 'RemoteCommand.php';
+require PATH_LIB . 'Invigilator.php';
 
-/*
-if (!RemoteCommand::validate($_GET)) {
-    echo RemoteCommand::getError();
-    exit;
-}
-
-
-//@todo change to use cache or file.
-
-$rows = RemoteCommand::getStatus($_GET['id']);
-if (VALUE_COMMON_ERROR == $rows) {
-    echo RemoteCommand::getError();
-    exit;
-}
-
-if (empty($rows)) {
-    $status = RemoteCommand::CMD_STATUS_NULL;
-} else {
-    $status = $rows[0]['status'];
-}
-
-//if new command exists on current request, reset it to 'not need'.
-if ($status == RemoteCommand::CMD_STATUS_EXIST) {
-    $ret = RemoteCommand::delCommand($_GET['id']);
-    if (VALUE_COMMON_ERROR == $ret) {
-        echo json_encode(RemoteCommand::getError());
-        exit;
-    }
-}
-
-echo $status;
-*/
-
-function validate()
-{
-    if (!isset($_GET['id'])) {
-        return false;
-    }
-
-    $id = trim($_GET['id']);
-    if (empty($id)) {
-        return false;
-    }
-
-    return $_GET;
-}
-$data = validate();
-if (false === $data) {
+if (!isset($_GET['patient_id'])) {
     echo 'param error.';
     exit;
 }
-$file = PATH_CACHE_CMD . $data['id'] . '.php';
+$patientId = $_GET['patient_id'];
+
+$file = PATH_CACHE_CMD . $patientId . '.php';
+$invigilator = new Invigilator($patientId);
+$command = $invigilator->getCommand();
+
 $result = array();
-if (file_exists($file)) {
-    include $file;
-    if (isset($command['end_time'])) {
-        if (time() >= $command['end_time']) {
-            $commandNew['action'] = 'end';
-        }
-    }
-    $result['code'] = 0;
-    $result['command'] = $commandNew;
-    
-    //@todo delete new command here.
-} else {
+if (empty($command)) {
     $result['code'] = 9;
+} else {
+    $result['code'] = 0;
+    $result['command'] = $command;
+    $invigilator->clearCommand();
 }
+
 echo json_encode($result);
---
+++ lib/Invigilator.php
@@ -0,0 +1,141 @@
+<?php
+class Invigilator
+{
+    private $info = array();
+    private $allowCommands = array(
+            'action', 'card', 'all_time', 'check_info',
+            'mode1_polycardia', 'mode1_bradycardia', 'mode1_lead',
+            'mode2_record_time', 'mode2_polycardia', 'mode2_bradycardia',
+            'mode2_regular_time', 'mode2_premature_beat', 'mode2_arrhythmia',
+            'mode2_pacemaker', 'mode2_lead',
+            'mode3_polycardia', 'mode3_bradycardia', 'mode3_lead', 'mode3_record_time');
+    private $commands = array();
+    private $file = '';
+    private $id;
+    
+    public function __construct($patientId, $mode = '0')
+    {
+        $this->id = $patientId;
+        $this->file = PATH_CACHE_CMD . $this->id . '.php';
+        if (file_exists($this->file)) {
+            include $this->file;
+            if ($info['status'] == 2) {
+                unlink($this->file);
+                $this->setDefaultInfo($mode);
+            } else {
+                $this->info = $info;
+                $this->commands = $command;
+            }
+        } else {
+            $this->setDefaultInfo($mode);            
+        }
+    }
+    
+    public function clearCommand()
+    {
+        $this->commands = array();
+        $this->create($this->id, array());
+    }
+    
+    public function getCommand()
+    {
+        if (!empty($this->info['end_time'])) {
+            if (time() >= $this->info['end_time']) {
+                $this->commands['action'] = 'end';
+            }
+        }
+        return $this->commands;
+    }
+    
+    public function create(array $data)
+    {
+        $commandKeys = array_intersect($this->allowCommands, array_keys($data));
+        foreach ($commandKeys as $cmd) {
+            $this->commands[$cmd] = $data[$cmd];
+        }
+        
+        foreach ($data as $key => $value) {
+            $this->info[$key] = $value;
+        }
+        
+        if ($this->info['mode'] == 3 && isset($this->commands['action'])) {
+            unset($this->commands['action']);
+        }
+        if (isset($data['action'])) {
+            $this->setStatuAndEndTime($data['action']);
+        }
+        
+        $template = $this->getTemplateInfo() . $this->getTemplateCmd();
+        
+        $handle = fopen($this->file, 'w');
+        fwrite($handle, $template);
+        fclose($handle);
+    }
+    
+    private function setDefaultInfo($mode)
+    {
+        $this->$info['mode'] = '0';
+        $this->$info['status'] = ($mode == '3') ? '3' : '0';
+        $this->$info['card'] = 'master';
+        $this->$info['all_time'] = 24;
+        $this->$info['check_info'] = 'off';
+        $this->$info['mode1_polycardia'] = 120;
+        $this->$info['mode1_bradycardia'] = 50;
+        $this->$info['mode1_lead'] = 'V5';
+        $this->$info['mode2_record_time'] = 20;
+        $this->$info['mode2_polycardia'] = 120;
+        $this->$info['mode2_bradycardia'] = 50;
+        $this->$info['mode2_regular_time'] = 1;
+        $this->$info['mode2_premature_beat'] = 8;
+        $this->$info['mode2_arrhythmia'] = 'on';
+        $this->$info['mode2_pacemaker'] = 'on';
+        $this->$info['mode2_lead'] = 'V5';
+        $this->$info['mode3_polycardia'] = 120;
+        $this->$info['mode3_bradycardia'] = 50;
+        $this->$info['mode3_lead'] = 'V5';
+        $this->$info['mode3_record_time'] = 20;
+        $this->$info['start_time'] = '';
+        $this->$info['end_time'] = '';
+    }
+    
+    private function setStatuAndEndTime($action)
+    {
+        if ($this->info['mode'] != 1 && $this->info['mode'] != 2) {
+            return;
+        }
+        if ($action == 'start' && $this->info['end_time'] == '') {
+            $this->info['status'] = 1;
+            $this->info['start_time'] = time();
+            $this->info['end_time'] = $this->info['start_time']
+                + $this->info['all_time'] * 3600;
+        }
+        if ($action == 'end') {
+            $this->info['status'] = 2;
+            $this->info['start_time'] = '';
+            $this->info['end_time'] = '';
+        }
+    }
+    
+    private function getTemplateInfo()
+    {
+        $template = "<?php\n";
+        $template .= '$info = array();' . "\n";
+        
+        foreach ($this->info as $key => $value) {
+            $template .= '$info[\'mode\'] = \'' . $this->info['mode'] . "';\n";
+            $template .= "\$info['$key'] = '$value';\n";
+        }
+        $template .= "\n";
+        
+        return $template;
+    }
+    
+    private function getTemplateCmd()
+    {
+        $template = "\$command = array();\n";
+        foreach ($this->commands as $key => $value) {
+            $template .= "\$command['$key'] = '$value';\n";
+        }
+        return $template;
+    }
+}
