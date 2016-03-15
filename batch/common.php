6b165c23c81013e43a9fa95a3de27f656ee52028
hlk666 <hupeng1982@126.com>
2016-03-15 09:01:19 +0800

    (add)create common file of batch.

---
+++ batch/common.php
@@ -0,0 +1,7 @@
+<?php
+//require 'E:\wamp\www\config\config.php';
+require 'D:\hp\www\yizhong\config\config.php';
+require PATH_LIB . 'Logger.php';
+require PATH_LIB . 'function.php';
+require PATH_LIB . 'DbiAdmin.php';
+require PATH_LIB . 'DataFile.php';
