57d30c238a06501e6d875fb6e9fbc9b8f58621e0
hlk666 <hupeng1982@126.com>
2018-02-26 13:16:03 +0800

    save and query device_fault.

---
+++ api/check/add_device_fault.php
@@ -0,0 +1,17 @@
+<?php
+require_once PATH_LIB . 'DbiAdmin.php';
+require_once PATH_LIB . 'Validate.php';
+
+if (false === Validate::checkRequired($_POST['device_id'])) {
+    api_exit(['code' => '1', 'message' => MESSAGE_REQUIRED . 'device_id.']);
+}
+if (false === Validate::checkRequired($_POST['fault'])) {
+    api_exit(['code' => '1', 'message' => MESSAGE_REQUIRED . 'fault.']);
+}
+
+$ret = DbiAdmin::getDbi()->addDeviceFault($_POST['device_id'], $_POST['fault']);
+if (VALUE_DB_ERROR === $ret) {
+    api_exit(['code' => '2', 'message' => MESSAGE_DB_ERROR]);
+}
+
+api_exit_success();
