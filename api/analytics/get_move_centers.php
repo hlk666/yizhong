136a1ef6d81244c8fe9a36e3e8b151e54cd75695
hlk666 <hupeng1982@126.com>
2018-04-16 16:53:06 +0800

    add Yizhong cloud2.

--- api/analytics/get_move_centers.php
+++ api/analytics/get_move_centers.php
@@ -3,8 +3,10 @@ $ret = array();
 $ret['code'] = 0;
 $hospital1 = ['hospital_id' => '119', 'hospital_name' => '羿中云平台'];
 $hospital2 = ['hospital_id' => '132', 'hospital_name' => 'YZ-S'];
+$hospital3 = ['hospital_id' => '141', 'hospital_name' => '羿中云平台2'];
 $ret['hospitals'][] = $hospital1;
 $ret['hospitals'][] = $hospital2;
+$ret['hospitals'][] = $hospital3;
 
 analytics_exit($ret);
 
