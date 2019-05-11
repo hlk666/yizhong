a7e757e01dc4a4043adc2a68bf0864e57b9dfbb1
hlk666 <hupeng1982@126.com>
2019-05-11 13:32:43 +0800

    fix bug.

--- api/client/notice_upload_data.php
+++ api/client/notice_upload_data.php
@@ -56,6 +56,20 @@ if (VALUE_DB_ERROR === $ret) {
     api_exit(['code' => '2', 'message' => MESSAGE_DB_ERROR]);
 }
 
+$noticeHospital1 = '0';
+$noticeHospital2 = '0';
+$tree = DbiAnalytics::getDbi()->getHospitalTree($guardianId);
+if (VALUE_DB_ERROR === $tree || array() == $tree) {
+    //do nothing.
+} else {
+    //setNotice($tree['analysis_hospital'], 'upload_data', $guardianId);
+    $noticeHospital1 = $tree['analysis_hospital'];
+    if ($tree['hospital_id'] != $tree['report_hospital']) {
+        //setNotice($tree['report_hospital'], 'upload_data', $guardianId);
+        $noticeHospital2 = $tree['report_hospital'];
+    }
+}
+/*
 if (1 == $deviceType) {
     $ret = DbiAdmin::getDbi()->appUploadSucceed($guardianId);
     if (VALUE_DB_ERROR === $ret) {
@@ -77,35 +91,55 @@ if (1 == $deviceType) {
         }
     }
 }
+*/
 
-moveData($guardianId);
+$hospitalNotMoveDate = [630, 631, 632];
+if (!in_array($hospitalId, $hospitalNotMoveDate)) {
+    $isNoticed = moveData($guardianId);
+    if (!$isNoticed) {
+        if ($noticeHospital1 != '0') {
+            setNotice($noticeHospital1, 'upload_data', $guardianId);
+        }
+        if ($noticeHospital2 != '0') {
+            setNotice($noticeHospital2, 'upload_data', $guardianId);
+        }
+    }
+} else {
+    if ($noticeHospital1 != '0') {
+        setNotice($noticeHospital1, 'upload_data', $guardianId);
+    }
+    if ($noticeHospital2 != '0') {
+        setNotice($noticeHospital2, 'upload_data', $guardianId);
+    }
+}
 
 api_exit_success();
 
 function moveData($patientId)
 {
+    $isNoticed = false;
     $hospitalConfig = DbiAnalytics::getDbi()->getReportHospitalByPatient($patientId);
     if ($hospitalConfig === VALUE_DB_ERROR) {
         Logger::write('move_data.log', 'db error when ' . $patientId);
-        return;
+        return false;
     }
     if (empty($hospitalConfig) || $hospitalConfig['report_hospital'] == $hospitalConfig['hospital_id']) {
-        return;
+        return false;
     }
     //滨医心内科
     if ($hospitalConfig['hospital_id'] == '203') {
-        return;
+        return false;
     }
     $hospitalId = $hospitalConfig['report_hospital'];
     //format of file :1,4,10.current index is 1, 4 of 10 need be moved.
     $file = PATH_CONFIG . 'move_data' . DIRECTORY_SEPARATOR . $hospitalId . '.txt';
     if (!file_exists($file)) {
-        return;
+        return false;
     }
     $config = explode(',', file_get_contents($file));
     if (count($config) != 3) {
         Logger::write('move_data.log', 'config error when ' . $patientId);
-        return;
+        return false;
     }
     $index = $config[0];
     $limit = $config[1];
@@ -114,10 +148,10 @@ function moveData($patientId)
     if ($index <= $limit) {
         $ret = DbiAnalytics::getDbi()->moveData($patientId, $hospitalId, '119', '1', '2');
         if (VALUE_DB_ERROR === $ret) {
-            api_exit(['code' => '2', 'message' => MESSAGE_DB_ERROR]);
+            return false;
         }
         
-        clearNotice($hospitalId, 'upload_data', $patientId);
+        //clearNotice($hospitalId, 'upload_data', $patientId);
         
         $file1 = PATH_ROOT . 'data' . DIRECTORY_SEPARATOR . 'move_data' . DIRECTORY_SEPARATOR . $hospitalId . '.txt';
         
@@ -130,12 +164,10 @@ function moveData($patientId)
             $text = '';
         }
         $text .= $patientId;
-        
-        $handle = fopen($file1, 'w');
-        fwrite($handle, $text);
-        fclose($handle);
+        file_put_contents($file1, $text);
         
         setNotice('119', 'move_data', $patientId);
+        $isNoticed = true;
     } else {
         //not move data
     }
@@ -148,4 +180,5 @@ function moveData($patientId)
     $newConfig = $index . ',' . $limit . ',' . $total;
     file_put_contents($file, $newConfig);
     Logger::write('move_data.log', 'data moved with ID: ' . $patientId);
+    return $isNoticed;
 }
