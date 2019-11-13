78a9d22bb04d5822b419c5f61c0aea56c1e7b42b
hlk666 <hupeng1982@126.com>
2019-11-13 08:04:52 +0800

    fix bug.clear notice when report uploaded.

--- lib/analysis/AnalysisUpload.php
+++ lib/analysis/AnalysisUpload.php
@@ -99,19 +99,22 @@ class AnalysisUpload
             }
         }
         
-        //special action for zhongda start.
         if ($type == 'report') {
             $tree = DbiAnalytics::getDbi()->getHospitalTree($guardianId);
             if (VALUE_DB_ERROR !== $tree && array() !== $tree) {
+                clearNotice($tree['analysis_hospital'], 'upload_data', $guardianId);
+                clearNotice($tree['report_hospital'], 'upload_data', $guardianId);
+                
+                //special action for zhongda start.
                 if (in_array($tree['report_hospital'], [132])) {
                     $ret = DbiAnalytics::getDbi()->setZhongdaData($guardianId);
                     if (VALUE_DB_ERROR === $ret) {
                         Logger::write('zhongda.log', "zhongda error.");
                     }
                 }
+                //special action for zhongda end.
             }
         }
-        //special action for zhongda end.
         
         return json_encode($this->retSuccess);
     }
