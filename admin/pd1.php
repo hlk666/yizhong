9528172f3ba8ee5096fac3fceb73e91647dd7105
hlk666 <hupeng1982@126.com>
2019-11-23 13:18:06 +0800

    add version info to pd.

--- admin/pd1.php
+++ admin/pd1.php
@@ -8,9 +8,17 @@ $isHideSider = true;
 require 'header.php';
 
 if (isset($_POST['submit'])){
-    $ret = DbiAdmin::getDbi()->addDevicePD($_POST['device']);
-    if (VALUE_DB_ERROR === $ret) {
+    $deviceId = $_POST['device'];
+    $isExisted = DbiAdmin::getDbi()->existedDevice2($deviceId);
+    if (VALUE_DB_ERROR === $isExisted) {
         user_back_after_delay(MESSAGE_DB_ERROR);
+    } elseif (true === $isExisted) {
+        user_back_after_delay("设备【 $deviceId 】已绑定其他医院或代理商/业务员，请从【发货/调配】页面操作。");
+    } else {
+        $ret = DbiAdmin::getDbi()->addDevicePD($deviceId);
+        if (VALUE_DB_ERROR === $ret) {
+            user_back_after_delay(MESSAGE_DB_ERROR);
+        }
     }
 }
 
@@ -20,20 +28,37 @@ if (VALUE_DB_ERROR === $deviceList) {
 }
 
 $htmlDevices = '';
-$optionVersion = '<option value="0">请选择版本</option>' 
-        . '<option value="app:1.0,采集盒:2.0,嵌入式3.0">app:1.0,采集盒:2.0,嵌入式3.0</option>'
-        . '<option value="app:1.1,采集盒:2.1,嵌入式3.1">app:1.1,采集盒:2.1,嵌入式3.1</option>';
+$optionVersionPhone = '<option value="0">请选择</option>' 
+        . '<option value="红米5A">红米5A</option>'
+        . '<option value="红米7A">红米7A</option>';
+$optionVersionEmbedded = '<option value="3.0(调整写卡看门狗)">调整写卡看门狗</option>';
+//'<option value="0">请选择</option>' . '<option value="3.0(调整写卡看门狗)">3.0(调整写卡看门狗)</option>';
+$optionVersionApp = '<option value="18.0(蓝牙调整)">蓝牙调整</option>';
+//'<option value="0">请选择</option>' . '<option value="0.1(test)">0.1(test)</option>';
+$optionVersionPcb = '<option value="0">请选择</option>'
+        . '<option value="20180801">20180801</option>'
+        . '<option value="返修">返修</option>';
+$optionVersionBox = '<option value="YZXD801-2">YZXD801-2</option>';
+//'<option value="0">请选择</option>' . '<option value="白盒方形">白盒方形</option>';
+
 foreach ($deviceList as $value) {
     $btnDel = '<button type="button" class="btn btn-xs btn-info" onclick="javascript:pdFunc('. $value['device_id'] . ', \'delete\')">注销</button>';
     $btnAbandon = '<button type="button" class="btn btn-xs btn-info" onclick="javascript:pdFunc('. $value['device_id'] . ', \'abandon\')">移入废品库</button>';
     $btnWarehouse = '<button type="button" class="btn btn-xs btn-info" onclick="javascript:pdFunc('. $value['device_id'] . ', \'warehouse\')">移入成品库</button>';
-    $selectVersion = '<select class="form-control" id="version' . $value['device_id'] . '">' . $optionVersion . '</select>'; 
-    $htmlDevices .= '<tr><td>' . $value['device_id'] . '</td><td>' . $btnDel . '</td><td>' . 
-        $btnAbandon . '</td><td>' . $selectVersion . '</td><td>' . $btnWarehouse . '</td></tr>';
+    $selectVersionPhone = '<select class="form-control" id="version_phone' . $value['device_id'] . '">' . $optionVersionPhone . '</select>';
+    $selectVersionEmbedded = '<select class="form-control" id="version_embedded' . $value['device_id'] . '">' . $optionVersionEmbedded . '</select>';
+    $selectVersionApp = '<select class="form-control" id="version_app' . $value['device_id'] . '">' . $optionVersionApp . '</select>';
+    $selectVersionPcb = '<select class="form-control" id="version_pcb' . $value['device_id'] . '">' . $optionVersionPcb . '</select>';
+    $selectVersionBox = '<select class="form-control" id="version_box' . $value['device_id'] . '">' . $optionVersionBox . '</select>';
+    $htmlDevices .= '<tr><td>' . $value['device_id'] . '</td><td>' . $btnDel . '</td><td>' . $btnAbandon 
+                . '</td><td>' . $selectVersionPhone . '</td><td>' . $selectVersionEmbedded 
+                . '</td><td>' . $selectVersionApp . '</td><td>' . $selectVersionPcb 
+                . '</td><td>' . $selectVersionBox 
+                . '</td><td>' . $btnWarehouse . '</td></tr>';
 }
 
 echo <<<EOF
-<h4>将设备添加到生产部(新品、退货等)。</h4>
+<h4>将设备添加到生产部(新品)。</h4>
 <form class="form-horizontal" role="form" method="post">
 <div class="row">
   <div class="col-xs-12 col-sm-3" style="margin-bottom:3px;">
@@ -52,10 +77,10 @@ echo <<<EOF
 <thead>
   <tr>
     <th>设备ID</th>
-    <th>注销/删除设备ID(不移入废品库)</th>
+    <th>注销/删除设备ID</th>
     <th>移入废品库</th>
-    <th>版本</th>
-    <th>移入成品库(选择版本后再操作)</th>
+    <th>手机版本</th><th>嵌入式版本</th><th>APP版本</th><th>电路板批次</th><th>记录盒型号</th>
+    <th>移入成品库</th>
   </tr>
 </thead>
 <tbody>$htmlDevices</tbody>
@@ -63,13 +88,19 @@ echo <<<EOF
 <script>
     function pdFunc(id, type)
     {
-        var version = document.getElementById("version" + id).value;
-        if (type == "warehouse" && version == "0") {
-            alert("请选择版本后再移入成品库。");
+        var version_phone = document.getElementById("version_phone" + id).value;
+        var version_embedded = document.getElementById("version_embedded" + id).value;
+        var version_app = document.getElementById("version_app" + id).value;
+        var version_pcb = document.getElementById("version_pcb" + id).value;
+        var version_box = document.getElementById("version_box" + id).value;
+        if (type == "warehouse" && (version_phone == "0" || version_embedded == "0"
+            || version_app == "0" || version_pcb == "0" || version_box == "0")) {
+            alert("请选择版本、型号后再移入成品库。");
             return;
         }
-        alert(version);
-        //window.location = 'pd_js.php?id=' + id + '&type=' + type + '&version=' + version;
+        //alert(version_phone + "," + version_embedded+ "," + version_app+ "," + version_pcb + "," + version_box);
+        window.location = 'pd_js.php?id=' + id + '&type=' + type + '&ver_phone=' + version_phone 
+        + '&ver_embedded=' + version_embedded + '&ver_app=' + version_app+ '&ver_pcb=' + version_pcb+ '&ver_box=' + version_box;
     }
 </script>
 EOF;
--- admin/pd_js.php
+++ admin/pd_js.php
@@ -10,10 +10,17 @@ require 'header.php';
 if (empty($_GET['id']) || empty($_GET['type'])) {
     user_back_after_delay('非法访问。');
 }
-$version = isset($_GET['version']) && !empty($_GET['version']) ? $_GET['version'] : '';
+//$version = isset($_GET['version']) && !empty($_GET['version']) ? $_GET['version'] : '';
 $iccid = isset($_GET['iccid']) && !empty($_GET['iccid']) ? $_GET['iccid'] : '';
+
+$vPhone = isset($_GET['ver_phone']) && !empty($_GET['ver_phone']) ? $_GET['ver_phone'] : '';
+$vEmbedded = isset($_GET['ver_embedded']) && !empty($_GET['ver_embedded']) ? $_GET['ver_embedded'] : '';
+$vApp = isset($_GET['ver_app']) && !empty($_GET['ver_app']) ? $_GET['ver_app'] : '';
+$vPcb = isset($_GET['ver_pcb']) && !empty($_GET['ver_pcb']) ? $_GET['ver_pcb'] : '';
+$vBox = isset($_GET['ver_box']) && !empty($_GET['ver_box']) ? $_GET['ver_box'] : '';
+
 $func = 'pd' . ucwords($_GET['type']);
-$ret = DbiAdmin::getDbi()->$func($_GET['id'], $_SESSION['user'], $version, $iccid);
+$ret = DbiAdmin::getDbi()->$func($_GET['id'], $_SESSION['user'], $iccid, $vPhone, $vEmbedded, $vApp, $vPcb, $vBox);
 if (VALUE_DB_ERROR === $ret) {
     user_back_after_delay(MESSAGE_DB_ERROR);
 }
