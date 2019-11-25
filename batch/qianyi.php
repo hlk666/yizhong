007cb64f8b4f614b17cbd944dcb74e88b72006a6
hlk666 <hupeng1982@126.com>
2019-11-25 15:40:49 +0800

    move request function to common file.

--- batch/qianyi.php
+++ batch/qianyi.php
@@ -108,16 +108,3 @@ foreach ($ret as $row) {
 
 //echo 'ok';
 exit(0);
-
-function request($url, $post)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
-
--- lib/function.php
+++ lib/function.php
@@ -140,3 +140,14 @@ function getPaging($page, $lastPage, $currentPage = null) {
 //     }
 //     return true;
 // }
+function request($url, $post)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
