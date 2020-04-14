1f57c2a08a35baf3e1ab8d33bb9989ae53592623
hlk666 <hupeng1982@126.com>
2020-04-14 14:31:27 +0800

    add user param to get_ecg_by_guard_hospital.

--- api/check/get_ecg_by_guard_hospital.php
+++ api/check/get_ecg_by_guard_hospital.php
@@ -4,7 +4,13 @@ require_once PATH_LIB . 'Validate.php';
 if (false === Validate::checkRequired($_GET['hospital'])) {
     api_exit(['code' => '1', 'message' => MESSAGE_REQUIRED . 'hospital.']);
 }
-$file = PATH_ROOT . 'cache' . DIRECTORY_SEPARATOR . 'ecg_data' . DIRECTORY_SEPARATOR . $_GET['hospital'] . '.txt';
+$user = isset($_GET['user']) && !empty($_GET['user']) ? $_GET['user'] : null;
+if (empty($user)) {
+    $file = PATH_ROOT . 'cache' . DIRECTORY_SEPARATOR . 'ecg_data' . DIRECTORY_SEPARATOR . $_GET['hospital'] . '.txt';
+} else {
+    $file = PATH_ROOT . 'cache' . DIRECTORY_SEPARATOR . 'ecg_data' . DIRECTORY_SEPARATOR . $_GET['hospital'] . '_' . $user . '.txt';
+}
+
 if (file_exists($file)) {
     $data = file_get_contents($file);
 } else {
