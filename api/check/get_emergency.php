89b5568bba66a57446fc5d5ac24fd3eb3263c496
hlk666 <hupeng1982@126.com>
2021-01-08 14:38:34 +0800

    new api.

---
+++ api/check/get_emergency.php
@@ -0,0 +1,18 @@
+<?php
+require_once PATH_LIB . 'DbiAdmin.php';
+
+$status = isset($_GET['status']) && !empty($_GET['status']) ? $_GET['status'] : null;
+
+$ret = DbiAdmin::getDbi()->getEmergency($status);
+if (VALUE_DB_ERROR === $ret) {
+    api_exit(['code' => '2', 'message' => MESSAGE_DB_ERROR]);
+}
+if (empty($ret)) {
+    api_exit(['code' => '4', 'message' => MESSAGE_DB_NO_DATA]);
+} else {
+    $result = array();
+    $result['code'] = '0';
+    $result['message'] = MESSAGE_SUCCESS;
+    $result['list'] = $ret;
+    api_exit($result);
+}
