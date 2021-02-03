0f1df0a04f8c0345537ea8fa946b371a8efc5044
hlk666 <hupeng1982@126.com>
2021-02-03 13:38:49 +0800

    set ssl of curl

--- lib/function.php
+++ lib/function.php
@@ -147,6 +147,7 @@ function request($url, $post)
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $data = curl_exec($ch);
     curl_close($ch);
     return $data;
